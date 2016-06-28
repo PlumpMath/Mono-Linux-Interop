b8103927ae14e0d1adf29c72e4694bb9e8c12cc5
Gokhan Sengun <gokhansengun@gmail.com>
2016-06-28 11:12:26 +0300

    Add mono interop console project to demo simplest case.

---
+++ Factorial.Mono/Factorial.Mono/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Factorial.Mono
+{
+	class InteropFunc
+	{
+		[DllImport ("libfact.so")]
+		public static extern long CalcFactorial(long n);
+	}
+
+	class UseCases
+	{
+		public static void SimplestUseCase()
+		{
+			for (int i = 1; i < 10; ++i) 
+			{
+				Console.WriteLine ("Factorial of {0} is {1}", i, InteropFunc.CalcFactorial (i));
+			}
+		}
+	}
+
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			UseCases.SimplestUseCase();
+		}
+	}
+}
